4e318745685bad2c794e1a4ab896a2d6ee4cde3a
simcothreadmills <simcochennai@gmail.com>
2021-06-14 11:49:13 +0530

    v2

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCHw4mQRPr43I_3RrlaiS6qL0Znjnc1Tug",
+    authDomain: "simco-53456.firebaseapp.com",
+    projectId: "simco-53456",
+    storageBucket: "simco-53456.appspot.com",
+    messagingSenderId: "637804967906",
+    appId: "1:637804967906:web:37605494e869aacecd6bbb",
+    measurementId: "G-JL1C0NV56V"
+  };
